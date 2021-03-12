a5bf58f057a1c5aef9626ab4733085f96e75d362
Sean O'Connor <sfoconnor1999@gmail.com>
2021-03-11 20:56:01 -0600

    Red Light |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -1,9 +1,6 @@
 def _create(userParms):
     result = {'grid': '0000000000000000', 'score': '0', 'integrity': '', 'status': 'ok'}
     twoCount = 0
-    while (len(result.get('grid')) < 16):
-        if(twoCount == 2):
-            result.get('grid') = result.get('grid') + 1
-        else:
+        
                
     return result
