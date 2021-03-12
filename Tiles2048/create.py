51b7d24dd5907642830158a2a76c8a6d9e4d4093
Sean O'Connor <sfoconnor1999@gmail.com>
2021-03-11 21:09:33 -0600

    Red Light |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -1,6 +1,14 @@
+import random
+
 def _create(userParms):
     result = {'grid': '0000000000000000', 'score': '0', 'integrity': '', 'status': 'ok'}
-    twoCount = 0
-        
+    ranIndexOne = random.randint(0, 15)
+    ranindexTwo = random.randint(0, 15)
+    while (ranIndexOne == ranindexTwo):
+        ranindexTwo = random.randint(0, 15)
+    tempGrid = str(result.get('grid'))
+    tempGrid.index(ranIndexOne) = "2"
+    tempGrid.index(ranindexTwo) = "2"
+    result.get('grid') = tempGrid
                
     return result
