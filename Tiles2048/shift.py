e899bc594161e11503efc5295cd615db1ea1d610
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-01 20:59:45 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -1,9 +1,4 @@
 def _shift(userParms):
-    result = userParms
-    grid = result['grid']
-    score = result['score']
-    integrity = result['integrity']
-    direction = result['direction']
-    
+    result = {'stub': 'stub result'}
     
     return result
