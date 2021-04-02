47067853aaded40d388e987b7a7d19fde56cd21d
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-01 23:12:59 -0500

    Red Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -5,10 +5,11 @@ class shiftTest(unittest.TestCase):
     #reusing code for calculating hash256 from last assignment. 
     #Thus, we can assume its functionality is correct. 
     def test_shift_HappyPathTest010(self):
-        userParms = {'op': 'shift', 'grid':'0020000020000000', 'score': '0', 'direction': 'down', 
+        userParms = {'op': 'shift', 'grid':'20002000000000', 'score': '0', 'direction': 'up', 
                         'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
         actual = shift._shift(userParms)
-        self.assertEqual(actual.get('score'), '0')          
+        grid = actual.get('grid')
+        self.assertEqual(grid[0], '4')          
         
     def test_shift_HappyPathTest020(self):
         userParms = {'op': 'shift', 'grid':'0000004024402020', 'score': '4', 'direction': 'up', 
