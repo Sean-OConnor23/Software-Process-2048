587cde6fef953db78836ba3a0998a771c49a380d
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-01 23:18:07 -0500

    Green Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -9,7 +9,8 @@ class shiftTest(unittest.TestCase):
                         'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
         actual = shift._shift(userParms)
         grid = actual.get('grid')
-        self.assertEqual(grid[0], '4')          
+        val = str(grid[0])
+        self.assertEqual(val, '4')          
         
     def test_shift_HappyPathTest020(self):
         userParms = {'op': 'shift', 'grid':'0000004024402020', 'score': '4', 'direction': 'up', 
