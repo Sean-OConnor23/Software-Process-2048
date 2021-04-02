194a024fd2bd0f6879f496495c56cb98d7e8fbb3
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-01 22:50:54 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -3,11 +3,13 @@ def _shift(userParms):
     direction = result['direction']
     if (direction == 'up'):
         #Dictates which direction to break grid up into (columns here)
+        print("success")
     elif (direction == 'right' or direction == 'left'):
         #Dictates which direction to break grid into (rows here)
         print("success")
     else:
-        #We must assume it is down (columns here)
+        #We must assume it is an invalid direction
+        print("success")
         
         
     return result
