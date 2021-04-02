39803f4291b8528d32a94a0e2d5a41aa81f792e8
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-01 20:46:05 -0500

    Red Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -4,9 +4,9 @@ import Tiles2048.shift as shift
 class shiftTest(unittest.TestCase):
     
     def test_shift_HappyPathTest010(self):
-        userParms = {'op': 'shift', 'grid':'0020000020000000', 'score': '0', 'direction': 'down'
-                     , 'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
+        userParms = {'op': 'shift', 'grid':'0020000020000000', 'score': '0', 'direction': 'down', 
+                        'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
         actual = shift._shift(userParms)  
-        expected = {'grid': '0000004000002020', 'score': '0'
-                    , 'integrity': '0DA3DEE7C5D13224BA4937CCF213B29C57676C36CDFE1C5CFC86ED069C644A17', 'status': 'ok'}
+        expected = {'grid': '0000004000002020', 'score': '0',
+                     'integrity': '0DA3DEE7C5D13224BA4937CCF213B29C57676C36CDFE1C5CFC86ED069C644A17', 'status': 'ok'}
         self.assertEqual(actual, expected)           
