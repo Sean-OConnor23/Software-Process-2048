1a0a2c6d4167f24b848f0468189dc5ef36b2762b
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-01 22:46:56 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -1,4 +1,13 @@
 def _shift(userParms):
-    result = {'stub': 'stub result'}
-    
+    result = userParms
+    direction = result['direction']
+    if (direction == 'up'):
+        #Dictates which direction to break grid up into (columns here)
+    elif (direction == 'right' or direction == 'left'):
+        #Dictates which direction to break grid into (rows here)
+        print("success")
+    else:
+        #We must assume it is down (columns here)
+        
+        
     return result
--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -15,5 +15,39 @@ class shiftTest(unittest.TestCase):
                         'integrity': '2A2EF0D1BEA22B9D6AB67C482BFF954F93F6A3617EF052E11DD8776BFFB7325A'}
         actual = shift._shift(userParms)
         self.assertEqual(actual.get('score'), '16') 
-
+    #Tests to make sure input contains grid value 
+    def test_shift_SadPathTest010(self):
+        userParms = {'op': 'shift', 'score': '4', 'direction': 'up', 
+                        'integrity': '2A2EF0D1BEA22B9D6AB67C482BFF954F93F6A3617EF052E11DD8776BFFB7325A'}
+        actual = shift._shift(userParms)
+        expected = {'status': 'error: missing grid'}
+        self.assertEqual(actual, expected)
+    #Tests for an invalid grid length 
+    def test_shift_SadPathTest020(self):
+        userParms = {'op': 'shift', 'grid':'002000002000', 'score': '0', 'direction': 'down', 
+                        'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
+        actual = shift._shift(userParms)
+        expected = {'status': 'error: invalid grid'}
+        self.assertEqual(actual, expected)
+    #Tests for an invalid grid value 
+    def test_shift_SadPathTest030(self):
+        userParms = {'op': 'shift', 'grid':'0020000020000005', 'score': '0', 'direction': 'down', 
+                        'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
+        actual = shift._shift(userParms)
+        expected = {'status': 'error: invalid grid'}
+        self.assertEqual(actual, expected)
+    #Tests for a valid score 
+    def test_shift_SadPathTest040(self):
+        userParms = {'op': 'shift', 'grid':'002000002000', 'score': '1', 'direction': 'down', 
+                        'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
+        actual = shift._shift(userParms)
+        expected = {'status': 'error: invalid score'}
+        self.assertEqual(actual, expected)
+    #Tests for a valid direction 
+    def test_shift_SadPathTest050(self):
+        userParms = {'op': 'shift', 'grid':'002000002000', 'score': '0', 'direction': 'back', 
+                        'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
+        actual = shift._shift(userParms)
+        expected = {'status': 'error: invalid direction'}
+        self.assertEqual(actual, expected)
        
