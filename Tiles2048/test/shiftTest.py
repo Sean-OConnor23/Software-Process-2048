32665853ccaa548a106d8d873ddf3b8e29db449a
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-03 00:19:50 -0400

    Red Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -69,4 +69,9 @@ class shiftTest(unittest.TestCase):
         expected = {'status': 'error: bad integrity value'}
         self.assertEqual(actual, expected)
         
-       
+    def test_shift_HappyPathTest030(self):
+        userParms = {'op': 'shift', 'grid':'2222444488881616160', 'score': '9600', 'direction': 'left', 
+                    'integrity': '66457746F0596CEE48B4FA4FA9C57A8A56A917F5B42F2600F12CD4266B9098BE'}
+        actual = shift._shift(userParms)
+        score = str(actual.get('score'))
+        self.assertEqual(score, '9688') 
