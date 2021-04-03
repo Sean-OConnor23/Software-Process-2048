52a1d6454a9d436be1a03f3b60142cd35ff393f1
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-03 00:15:43 -0400

    Green Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -3,17 +3,21 @@ import hashlib
 
 def _shift(userParms):
     result = userParms
+    finalResult = {}
     search = 'grid'
+    tempGrid = ''
+    status = 'ok'
     validNum = [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
     if (not search in result):
         error = {'status': 'error: missing grid'}
         return error
-    grid = result.get('grid')
+    grid = result.get('grid')        
     direction = result.get('direction')
     if ((int(result['score'])) % 2 != 0):
         error = {'status': 'error: invalid score'}
         return error
-    if (len(str(grid)) != 16):
+    score = int(result.get('score'))
+    if (len(str(grid)) < 16):
         error = {'status': 'error: invalid grid'}
         return error
     for tempVal in grid:
@@ -27,19 +31,339 @@ def _shift(userParms):
       
     if (direction == 'up' or direction == 'down' or not('direction' in result)):
         #Dictates which direction to break grid up into (columns here)
-        
-        
-        print("success")
+        column1 = []
+        #column1val = [0, 4, 8, 12]
+        column2 = []
+        #column2val = [1, 5, 9, 13]
+        column3 = []
+        #column3val = [2, 6, 10, 14]
+        column4 = []
+        #column4val = [3, 7, 11, 15]
+        i = 1
+        idk = -1
+        skip = 0
+        for tempVal in grid:
+           
+            idk = idk + 1
+            length = len(grid)
+            if (idk == length - 1):
+                idk = idk - 2
+            tempVal = int(tempVal)
+            if (skip != 0):
+                skip = skip - 1
+                continue
+            if (tempVal == 0):
+                if (i == 4):
+                    i = 1
+                else:
+                    i = i + 1
+                continue
+            elif (tempVal == 1):
+                if (int(grid[idk + 1]) == 6):
+                    tempVal = 16
+                    skip = 1
+                else:
+                    tempVal = 128
+                    skip = 2
+            elif (tempVal == 2):
+                if (idk - 1 < len(grid)):
+                    
+                    if (int(grid[idk + 1]) == 5):
+                        tempVal = 256
+                        skip = 2         
+            elif (tempVal == 3):
+                if(int(grid[idk + 1]) == 2):
+                    tempVal = 32
+                    skip = 1
+            elif (tempVal == 5):
+                if (int(grid[idk+1]) == 1):
+                    tempVal = 512
+                    skip = 2
+            elif (tempVal == 6):
+                if (int(grid[idk + 1]) == 4):
+                    tempVal = 64
+                    skip = 1
+            if (i == 1):
+                column1.append(tempVal)
+                i = i + 1
+            elif(i == 2):
+                column2.append(tempVal)
+                i = i + 1
+            elif(i == 3):
+                column3.append(tempVal)
+                i = i + 1
+            else:
+                column4.append(tempVal)
+                i = 1
+        #Grid has now been added into proper columns 
+        columns = [column1, column2, column3, column4]
+        if (direction == 'up'):
+            #Perform UP
+            for column in columns:
+                while(len(column) != 4):
+                    column.append(0)
+                if (column[0] == column[1]):
+                    column[0] = column[0] + column[1]
+                    status = _checkWin(column[0])
+                    score = score + column[0]
+                    if (column[2] == column[3]):
+                        column[1] = column[2] + column[3]
+                        status = _checkWin(column[1])
+                        score = score + column[1]
+                        column[2] = 0
+                        column[3] = 0
+                        continue
+                    column[1] = column[2]
+                    column[2] = column[3]
+                    column[3] = 0
+                    continue
+                elif (column[1] == column[2]):
+                    column[1] = column[1] + column[2]
+                    status = _checkWin(column[1])
+                    score = score + column[1]
+                    column[2] = column[3]
+                    column[3] = 0
+                    continue
+                elif (column[2] == column[3]):
+                    column[2] = column[2] + column[3]
+                    status = _checkWin(column[2])
+                    score = score + column[2]
+                    column[3] = 0
+                    continue
+                    
+        else:
+            #Perform DOWN 
+            for column in columns:
+                while (len(column) != 4):
+                    column.insert(0, 0)
+                if (column[3] == column[2]):
+                    column[3] = column[3] + column[2]
+                    status = _checkWin(column[3])
+                    score = score + column[3]
+                    if (column[1] == column[0]):
+                        column[2] = column[1] + column[0]
+                        status = _checkWin(column[2])
+                        score = score + column[2]
+                        column[1] = 0
+                        column[0] = 0
+                        continue
+                    column[2] = column[1]
+                    column[1] = column[0]
+                    column[0] = 0
+                    continue
+                elif (column[2] == column[1]):
+                    column[2] = column[2] + column[1]
+                    status = _checkWin(column[2])
+                    score = score + column[2]
+                    column[1] = column[0]
+                    column[0] = 0
+                    continue
+                elif (column[1] == column[0]):
+                    column[1] = column[1] + column[0]
+                    status = _checkWin(column[1])
+                    score = score + column[1]
+                    column[0] = 0
+                    continue
+                
+        #Insert random 2/4 here 
+        added = False
+        while (added == False): 
+            colNum = random.randint(0, 3)
+            specNum = random.randint(0, 3)
+            comp = columns[colNum][specNum]
+            if (comp == 0):
+                whichOne = random.randint(0,1)
+                if (whichOne == 0):
+                    columns[colNum][specNum] = 2
+                else:
+                    columns[colNum][specNum] = 4
+                added = True
+                
+        #Need to check for available 0's
+        loss = False
+        for column in columns:
+            if (0 in column):
+                continue
+            else:
+                loss = True
+            
+        if (loss == True):
+            status = 'lose'
+            
+            
+        #Insert grid realignment here 
+        j = 0
+        while (j <= 3):
+            tempGrid = tempGrid + str(column1[j])
+            tempGrid = tempGrid + str(column2[j])
+            tempGrid = tempGrid + str(column3[j])
+            tempGrid = tempGrid + str(column4[j])
+            j = j + 1
+            
+                    
     elif (direction == 'right' or direction == 'left'):
         #Dictates which direction to break grid into (rows here)
-        
-        
-        print("success")
+        row1 = []
+        #row1val = [0, 1, 2, 3]
+        row2 = []
+        #row2val = [4, 5, 6, 7]
+        row3 = []
+        #row3val = [8, 9, 10, 11]
+        row4 = []
+        #row4val = [12, 13, 14, 15]
+        i = 1
+        idk = -1
+        skip = 0
+        for tempVal in grid:     
+            idk = idk + 1
+            tempVal = int(tempVal)
+            if (skip != 0):
+                skip = skip - 1
+                continue
+            if (tempVal == 0):
+                if (i == 4):
+                    i = 1
+                else:
+                    i = i + 1
+                continue
+            elif (tempVal == 1):
+                if (int(grid[idk + 1]) == 6):
+                    tempVal = 16
+                    skip = 1
+                else:
+                    tempVal = 128
+                    skip = 2
+            elif (tempVal == 2):
+                if (int(grid[idk + 1]) == 5):
+                    tempVal = 256
+                    skip = 2         
+            elif (tempVal == 3):
+                if(int(grid[idk + 1]) == 2):
+                    tempVal = 32
+                    skip = 1
+            elif (tempVal == 5):
+                if (int(grid[idk+1]) == 1):
+                    tempVal = 512
+                    skip = 2
+            elif (tempVal == 6):
+                if (int(grid[idk + 1]) == 4):
+                    tempVal = 64
+                    skip = 1
+                 
+            if (i <= 4):
+                row1.append(tempVal)
+                i = i + 1
+            elif(i <= 8):
+                row2.append(tempVal)
+                i = i + 1
+            elif(i <= 12):
+                row3.append(tempVal)
+                i = i + 1
+            else:
+                row4.append(tempVal)
+        rows = [row1, row2, row3, row4]
+        #Grid has now been added into proper rows 
+        if (direction == 'right'):
+            #Perform RIGHT
+            for row in rows:
+                while (len(row) != 4):
+                    row.insert(0, 0)
+                if (row[3] == row[2]):
+                    row[3] = row[3] + row[2]
+                    status = _checkWin(row[3])
+                    score = score + row[3]
+                    if (row[1] == row[0]):
+                        row[2] = row[1] + row[0]
+                        status = _checkWin(row[2])
+                        score = score + row[2]
+                        row[1] = 0
+                        row[0] = 0
+                        continue
+                    row[2] = row[1]
+                    row[1] = row[0]
+                    row[0] = 0
+                    continue
+                elif (row[2] == row[1]):
+                    row[2] = row[2] + row[1]
+                    status = _checkWin(row[2])
+                    score = score + row[2]
+                    row[1] = row[0]
+                    row[0] = 0
+                    continue
+                elif (row[1] == row[0]):
+                    row[1] = row[1] + row[0]
+                    status = _checkWin(row[1])
+                    score = score + row[1]
+                    row[0] = 0
+                    continue
+                                  
+        else:
+            #Perform LEFT
+            for row in rows:
+                while (len(row) != 4):
+                    row.append(0)
+                if (row[0] == row[1]):
+                    row[0] = row[0] + row[1]
+                    status = _checkWin(row[0])
+                    score = score + row[0]
+                    if (row[2] == row[3]):
+                        row[1] = row[2] + row[3]
+                        status = _checkWin(row[1])
+                        score = score + row[1]
+                        row[2] = 0
+                        row[3] = 0
+                        continue
+                    row[1] = row[2]
+                    row[2] = row[3]
+                    row[3] = 0
+                    continue
+                elif (row[1] == row[2]):
+                    row[1] = row[1] + row[2]
+                    status = _checkWin(row[1])
+                    score = score + row[1]
+                    row[2] = row[3]
+                    row[3] = 0
+                    continue
+                elif (row[2] == row[3]):
+                    row[2] = row[2] + row[3]
+                    status = _checkWin(row[2])
+                    score = score + row[2]
+                    row[3] = 0
+                    continue
+        #Insert random 2/4 here:
+        added = False
+        while (added == False): 
+            rowNum = random.randint(0, 3)
+            specNum = random.randint(0, 3)
+            if (rows[rowNum][specNum] == 0):
+                whichOne = random.randint(0,1)
+                if (whichOne == 0):
+                    rows[rowNum][specNum] = 2
+                else:
+                    rows[rowNum][specNum] = 4
+                added = True           
+        #Need to check to see if any available 0's
+        loss = False
+        for row in rows:
+            if (0 in row):
+                continue
+            else:
+                loss = True
+            
+        if (loss == True):
+            status = 'lose'
+        #Insert grid realignment here 
+        tempGrid = str(row1) + str(row2) + str(row3) + str(row4)
     else:
         error = {'status': 'error: invalid direction'}
         return error
+    
+    finalResult['grid'] = tempGrid
+    finalResult['score'] = str(score)
+    finalResult['integrity'] = _calcIntegrity(finalResult)
+    finalResult['status'] = status
         
-    return result
+    return finalResult
 
 
 #Calculate integrity in separate grid. Helps for testing purposes
@@ -51,3 +375,10 @@ def _calcIntegrity(result):
     myHash.update(toConvert.encode())
     myHashDigest = myHash.hexdigest()
     return myHashDigest.upper()
+
+def _checkWin(numberIn):
+    if (numberIn == 2048):
+        return 'win'
+    else:
+        return 'ok'
+        
--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -5,8 +5,8 @@ class shiftTest(unittest.TestCase):
     #reusing code for calculating hash256 from last assignment. 
     #Thus, we can assume its functionality is correct. 
     def test_shift_HappyPathTest010(self):
-        userParms = {'op': 'shift', 'grid':'20002000000000', 'score': '0', 'direction': 'up', 
-                        'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
+        userParms = {'op': 'shift', 'grid':'2000200000000000', 'score': '0', 'direction': 'up', 
+                        'integrity': '10469CE866023129A5BFB507A4D641F98C4566747136F24A920AA1C846844320'}
         actual = shift._shift(userParms)
         grid = str(actual.get('grid'))
         val = grid[0]
@@ -57,7 +57,7 @@ class shiftTest(unittest.TestCase):
     #Tests to see if shift is avaliable 
     def test_shift_SadPathTest060(self):
         userParms = {'op': 'shift', 'grid':'2222222222222222', 'score': '0', 'direction': 'down', 
-                        'integrity': '2A2EF0D1BEA22B9D6AB67C482BFF954F93F6A3617EF052E11DD8776BFFB7325A'}
+                        'integrity': '64AE524A2A51581672C65755DE55392C4CCD1793639361A160A5E4C97E55C45A'}
         actual = shift._shift(userParms)
         expected = {'status': 'error: no shift possible'}
         self.assertEqual(actual, expected)
