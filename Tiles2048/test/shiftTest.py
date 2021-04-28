738118556fc71a34a3d3229accfe4ce887929c94
Sean O'Connor <sfoconnor1999@gmail.com>
2021-04-28 02:45:55 -0500

    Red Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -89,7 +89,7 @@ class shiftTest(unittest.TestCase):
         actual = shift._shift(userParms)
         expected = {'status': 'error: invalid score'}
         self.assertEqual(actual, expected)
-     def test_shift_SadPathTest100(self):
+    def test_shift_SadPathTest100(self):
         userParms = {'op': 'shift', 'grid':'002000002000', 'score': '', 'direction': 'down', 
                         'integrity': '7CD5E3DEAB08FCAE8F64433DC4A63CC922571EBF60EE1D1938ADCD415FB760E5'}
         actual = shift._shift(userParms)
